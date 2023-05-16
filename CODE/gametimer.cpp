bab41adfb1b4cda8a8312931c02adc18d7c001a3
Daniel Henry <48395640+dnilhnry@users.noreply.github.com>
2023-05-16 15:15:42 +0100

    FIXED TIME ISSUE

--- CODE/gametimer.cpp
+++ CODE/gametimer.cpp
@@ -66,7 +66,7 @@ void GameTimer::mark()
 		}
 
 		// Gradually ease off the throttling
-		mdThrottledFrameTime *= 0.95;
+		mdThrottledFrameTime *= 0.1;
 
 		// Don't let throttle go faster than requested speed
 		if (mdThrottledFrameTime < mdMinimumFrameTime)
